5cdd1331f4bda9928fa3f0276bf49290f6d3d9f7
Niclas Kjäll-Ohlsson <niclasko@gmail.com>
2018-04-23 14:35:28 +0200

    Update Sudoku.js

--- Sudoku/js/Sudoku.js
+++ Sudoku/js/Sudoku.js
@@ -6,7 +6,7 @@ var Sudoku = function(domainSize) {
 		var domain = {};
 		var initDomain = function() {
 			for(var i=0; i<domainSize; i++) {
-				domain[i+1] = true;
+				domain[i+1] = 0;
 			}
 		};
 		initDomain();
@@ -23,11 +23,11 @@ var Sudoku = function(domainSize) {
 			return domain;
 		};
 		this.inDomain = function(value) {
-			return domain[value];
+			return domain[value] == 0;
 		};
 		this.updateDomain = function(value, inDomain) {
 			if(value) {
-				domain[value] = inDomain;
+				domain[value] += (inDomain ? -1 : 1);
 			}
 		};
 	};
@@ -43,21 +43,30 @@ var Sudoku = function(domainSize) {
 	};
 	
 	var updateDomains = function(_row, _column, value, inDomain) {
+		toUpdate = {};
 		rowStart = Math.floor(_row/3)*3;
 		columnStart = Math.floor(_column/3)*3;
 		for(var row=rowStart; row<(rowStart+3); row++) {
 			for(var column=columnStart; column<(columnStart+3); column++) {
-				board[row][column].updateDomain(value, inDomain);
-				updateVisibleCellDomain(row, column);
+				if(!toUpdate[row+";"+column]) {
+					toUpdate[row+";"+column] = {"row": row, "column": column};
+				}
 			}
 		}
 		for(var column=0; column<9; column++) {
-			board[_row][column].updateDomain(value, inDomain);
-			updateVisibleCellDomain(_row, column);
+			if(!toUpdate[_row+";"+column]) {
+				toUpdate[_row+";"+column] = {"row": _row, "column": column};
+			}
 		}
 		for(var row=0; row<9; row++) {
-			board[row][_column].updateDomain(value, inDomain);
-			updateVisibleCellDomain(row, _column);
+			if(!toUpdate[row+";"+_column]) {
+				toUpdate[row+";"+_column] = {"row": row, "column": _column};
+			}
+		}
+		var cells = Object.values(toUpdate);
+		for(var i in cells) {
+			board[cells[i].row][cells[i].column].updateDomain(value, inDomain);
+			updateVisibleCellDomain(cells[i].row, cells[i].column);
 		}
 	};
 	
@@ -75,14 +84,14 @@ var Sudoku = function(domainSize) {
 		}
 		if(value == null && board[row][column].value() != null) {
 			// Add previous cell value back to domains of related cells
+			var previousValue = board[row][column].value();
+			board[row][column].setValue(value);
 			updateDomains(
 				row,
 				column,
-				board[row][column].value(),
+				previousValue,
 				true
 			);
-			board[row][column].setValue(value);
-			updateVisibleCellDomain(row, column);
 		}
 		if(value) {
 			board[row][column].setValue(value);
@@ -96,4 +105,3 @@ var Sudoku = function(domainSize) {
 		}
 	};
 };
-
