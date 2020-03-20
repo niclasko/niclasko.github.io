b438eb92f465f696ea2ab244438ec037919e04f4
Niclas Kjäll-Ohlsson <tnk@equinor.com>
2020-03-20 13:54:54 +0100

    Add files via upload

--- CypherJS/js/GraphViewer.js
+++ CypherJS/js/GraphViewer.js
@@ -1,3 +1,4 @@
+// DAG editor: http://bl.ocks.org/rkirsling/5001347
 var GraphViewer = function(options) {
 	
 	var preprocessGraphData = function(nodes, links) {
@@ -55,7 +56,8 @@ GraphViewer.prototype.draw = function() {
 
 	var simulation = d3.forceSimulation()
 		.force("center", d3.forceCenter(width/2, height/2))
-		.force("collision", d3.forceCollide(radius*8))
+		.force("charge",d3.forceManyBody())
+		.force("collide", d3.forceCollide(70))
 		.force("link", d3.forceLink());
 		
 	//add zoom capabilities 
@@ -64,41 +66,84 @@ GraphViewer.prototype.draw = function() {
 
 	zoom_handler(svg);
 	
-	//add encompassing group for the zoom 
+	//add group for the zoom 
 	var g = svg.append("g")
 		.attr("class", "everything");
+	
+	var defs = g.append("svg:defs");
+
+	defs.selectAll("marker")
+		.data(["arrow"])
+		.enter().append("svg:marker")
+		.attr("class", "arrowhead")
+		.attr("id", String)
+		.attr("viewBox", "0 -5 10 10")
+		.attr("refX", 22)
+		.attr("markerWidth", 4)
+		.attr("markerHeight", 4)
+		.attr("orient", "auto-start-reverse")
+		.append("svg:path")
+		.attr("d", "M0,-5L10,0L0,5");
 
 	var link = g.append("g")
 		.attr("class", "links")
-		.selectAll("line")
+		.selectAll("g")
 		.data(this.graphData.links)
-		.enter().append("line");
+		.enter().append("g");
+
+	var line = link
+		.append("line")
+		.attr("marker-end",
+			function(d) {
+				return d.direction == "right" ||
+					d.direction == "both" ? "url(#arrow)" : null;
+			}
+		)
+		.attr("marker-start",
+			function(d) {
+				return d.direction == "left" ||
+					d.direction == "both" ? "url(#arrow)" : null;
+			}
+		);
 		
-	link.append("title")
+	line.append("title")
 		.text(function(d) { return JSON.stringify(d); });
+
+	var linkText = link.append("text")
+		.attr("text-anchor", "middle")
+		.text(d => d.type);
 
 	var node = g.append("g")
 		.attr("class", "nodes")
-		.selectAll("circle")
+		.selectAll("g")
 		.data(this.graphData.nodes)
-		.enter().append("circle")
-			.attr("r", 10)
-			.attr("fill", function(d) { return color(d.group); })
-			.on("dblclick", function(d) {
-				if(!d.dragged) {
-					delete d["fx"];
-					delete d["fy"];
-					d3.event.stopPropagation();
-				}
-			})
-			.call(d3.drag()
-				.on("start", dragstarted)
-				.on("drag", dragged)
-				.on("end", dragended)
-			);
+		.enter().append("g")
+		.call(d3.drag()
+			.on("start", dragstarted)
+			.on("drag", dragged)
+			.on("end", dragended)
+		);
 
-	node.append("title")
-		.text(function(d) { return JSON.stringify(d); });
+	node.append("circle")
+		.attr("r", 7)
+		.attr("fill", function(d) { return color(d.group); })
+		.on("dblclick", function(d) {
+			if(!d.dragged) {
+				delete d["fx"];
+				delete d["fy"];
+				d3.event.stopPropagation();
+			}
+		})
+		.append("title")
+		.text(function(d) { return JSON.stringify(d); });;
+
+	node.append("text")
+		.attr("class", "nodetext")
+		.text(function(d) {
+			return d.properties["name"];
+		})
+		.attr("x", 7)
+		.attr("y", 3);
 
 	simulation
 		.nodes(this.graphData.nodes)
@@ -108,15 +153,40 @@ GraphViewer.prototype.draw = function() {
 		.links(this.graphData.links);
 
 	function ticked() {
-		link
+		line
 			.attr("x1", function(d) { return d.source.x; })
 			.attr("y1", function(d) { return d.source.y; })
 			.attr("x2", function(d) { return d.target.x; })
 			.attr("y2", function(d) { return d.target.y; });
 
+		linkText
+			.attr("x", function(d) { return d.source.x+((d.target.x-d.source.x)/2)+3.5; })
+			.attr("y", function(d) { return d.source.y+((d.target.y-d.source.y)/2)+3.5; })
+			.attr("transform", function(d) {
+				var deltaX = d.target.x-d.source.x;
+				var deltaY = d.target.y-d.source.y;
+				var originX = d.source.x+(deltaX/2);
+				var originY = d.source.y+(deltaY/2);
+				var angle = Math.atan2(deltaY, deltaX)/(Math.PI/180);
+				if(Math.abs(angle) > 90) {
+					angle -= 180;
+				}
+				fitText(Math.sqrt(Math.pow(deltaX,2)+Math.pow(deltaY,2))-30, this, d.type);
+				return "rotate(" + angle + "," + originX + ", " + originY + ")";
+			});
+
 		node
-			.attr("cx", function(d) { return d.x; })
-			.attr("cy", function(d) { return d.y; });
+			.attr("transform", function(d) {
+			  return "translate(" + d.x + "," + d.y + ")";
+			});
+	}
+
+	function fitText(linkLength, svgTextElement, text) {
+		svgTextElement.textContent = text;
+		var i = text.length-3;
+		while(svgTextElement.getComputedTextLength() > linkLength && i >= 0) {
+			svgTextElement.textContent = text.substring(0,i--) + "...";
+		}
 	}
 
 	function dragstarted(d) {
